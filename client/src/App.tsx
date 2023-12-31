54796269dd4bbd18e3bc128b5fbc306ec9071a45
Omkar Lohar <omkarlohar91@gmail.com>
2023-12-31 14:34:00 +0530

    feat: Shadcn setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ const { toast } = useToast();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen w-full flex items-center justify-center">
+        <Button
+          variant="outline"
+          onClick={() => {
+            toast({
+              description: "Your message has been sent.",
+            });
+          }}
+        >
+          Show Toast
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Toaster/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
