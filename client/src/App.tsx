b44fb40e4283653be02724360ec0cff8cf206e87
Omkar Lohar <omkarlohar91@gmail.com>
2024-02-03 02:10:08 +0530

    feature: home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,23 @@
 import { Navigate, Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/home/Home";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import AuthPage from "./pages/auth/Auth";
 function App() {
   const userAuth = false;
   return (
-    <div className="h-screen w-full">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/auth"
-            element={
-              userAuth ? <Navigate to="/" replace /> : <div>Auth</div>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="h-screen w-full">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/auth"
+              element={userAuth ? <Navigate to="/" replace /> : <AuthPage/>}
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
