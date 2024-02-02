33c51031ea464dc1df7b8dfb882c87327ba69dd3
Omkar Lohar <omkarlohar91@gmail.com>
2024-02-03 02:14:07 +0530

    fix: theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,20 @@
 import { Navigate, Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/home/Home";
-import { ThemeProvider } from "@/components/theme/theme-provider";
 import AuthPage from "./pages/auth/Auth";
 function App() {
   const userAuth = false;
   return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <div className="h-screen w-full">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route
-              path="/auth"
-              element={userAuth ? <Navigate to="/" replace /> : <AuthPage/>}
-            />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </ThemeProvider>
+    <div className="h-screen w-full">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/auth"
+            element={userAuth ? <Navigate to="/" replace /> : <AuthPage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
